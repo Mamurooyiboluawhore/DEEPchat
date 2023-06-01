f91a9407e1fd5ff1aa665fcbc07847ba344ede49
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-06-01 18:01:17 +0000

    routes

--- DeepChatApp/route.py
+++ DeepChatApp/route.py
@@ -2,6 +2,22 @@ from flask import render_template
 from DeepChatApp import app
 from DeepChatApp.models import User, Todo
 
+
+@app.route('/landingP')
+def landingP
+	return render_template("landP.html")
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
+@app.route('/registeration')
+def registeration():
+	return render_template("registeration")
+	
 @app.route('/')
 def Home():
         return render_template("index.html")
+
+@app.route('/main')
+def about():
+	return render_template("about.html)
