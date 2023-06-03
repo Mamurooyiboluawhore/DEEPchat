496bd5d56cb8272cce05906c7bee58f7053ce700
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-06-03 09:25:43 +0100

    todolist function added and believed to be properly handled

--- main.py
+++ main.py
@@ -31,14 +31,36 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
    """
    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
+    ################################################################################# 
+    ##                      Here comes DeepChat Power                              ##
+    #################################################################################
+
+async def todolist(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+        This function gets/fetch info from google calender
+    
+    """
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=None)
+    pass
+
+
+
+
 if __name__ == "__main__":
     from sys import argv
 
     application = ApplicationBuilder().token(argv[1]).build()
+    
+    """
+        Command Handlers to handle /start, /help, /todolist and (more to come) commands
+    """
 
     start_handler = CommandHandler('start', start)
     help_handler = CommandHandler('help', help)
+    todolist_handler = CommandHandler('todolist', todolist)
     application.add_handler(start_handler)
     application.add_handler(help_handler)
+    application.add_handler(todolist)
 
     application.run_polling()
